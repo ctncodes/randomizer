600bd9cb5f043874020f8cbb1f4afe45870282cf
ctncodes <35818754+ctncodes@users.noreply.github.com>
2019-10-23 02:13:22 -0700

    It's Magic!

--- sketch.js
+++ sketch.js
@@ -1,12 +1,319 @@
-let roster = ["Chi","Ti","Tam";"Terra";"Ventus","Aqua"];
+// let roster = [{
+//   firstName: "xiaowei",
+//   lastName: "zheng",
+//   color: " green",
+//   animal: "babies",
+//   book: "the little girl at the window",
+//   movie: "eat drink man woman",
+//   game: "none",
+//   superHero: "none",
+//   randomFact: "don't drink coke"
+// }, {
+//   firstName: "Andy",
+//   lastName: "Wu",
+//   color: "Red",
+//   animal: "Kiwi",
+//   book: "Romance of the Three Kingdoms",
+//   movie: "Shrek 2",
+//   game: "Super Mario 64",
+//   superHero: "Static Shock",
+//   randomFact: "I've had jaw correction surgery"
+// }, {
+//   firstName: "Chris",
+//   lastName: "Weber",
+//   color: "green",
+//   animal: "cat",
+//   book: "Hitchhiker's Guide to the Galaxy",
+//   movie: "Spiderman Into The Spiderverse",
+//   game: "The Legend of Zelda Breath of the Wild",
+//   superHero: "Iron Man",
+//   randomFact: "I'm vegetarian"
+// }, {
+//   firstName: "Sumedh",
+//   lastName: "Vedanthi",
+//   color: "Blue",
+//   animal: "Crow",
+//   book: "Of Mice and Men",
+//   movie: "American Psycho",
+//   game: "Fruit Ninja",
+//   superHero: "Batman",
+//   randomFact: "im a black belt"
+// }, {
+//   firstname: "Kaicong",
+//   lastname: "Tang",
+//   color: "black",
+//   animal: "dog",
+//   movie: "iron man123",
+//   game: "csgo",
+//   superHero: "iron man",
+//   randomFact: "love gaming"
+// }, {
+//   firstName: "Hao",
+//   lastName: "Rong",
+//   color: "hex(#4285F4)/Bright blue",
+//   animal: "otter",
+//   book: "A Brief History of Time",
+//   movie: "Angry Birds 2",
+//   game: "Final Fantasy XIV",
+//   superHero: "Tony Stark",
+//   randomFact: "I like cats."
+// }, {
+//   firstName: "Sierra",
+//   lastName: "Roe",
+//   color: "purple",
+//   animal: "dog",
+//   book: "phantom tollbooth",
+//   movie: "Naussica of the Valley of the Wind",
+//   game: "pokemon heartgold",
+//   superHero: "captain marvel",
+//   randomFact: "can run 7 minute mile"
+// }, {
+//   firstName: "Leon",
+//   lastName: "Quach",
+//   color: "silver",
+//   animal: "penguins",
+//   book: "Letters to a Young Poet",
+//   movie: "Kung fu Hustle",
+//   game: "Pokemon Crystal Version",
+//   superHero: "Batman",
+//   randomFact: "I have two pet guinea pigs"
+// }, {
+//   firstName: "Chi",
+//   lastName: "Nguyen",
+//   color: "purple",
+//   animal: "bird",
+//   book: "Math Curse",
+//   movie: "Big Hero 6",
+//   game: "Checkers",
+//   superHero: "Big Hero 6 (team)",
+//   randomFact: "Lately this year, monsters have especially been occupying my mind, among other things."
+// }, {
+//   firstName: "Lydia",
+//   lastName: "Lim",
+//   color: "blue",
+//   animal: "dog",
+//   book: "No",
+//   movie: "me before you",
+//   game: "No",
+//   superHero: "Thor",
+//   randomFact: " birds are tetrapods even though they only walk on two legs"
+// }, {
+//   firstName: "Matteo",
+//   lastName: "Leva",
+//   color: "Black",
+//   animal: "Cat",
+//   book: "Pillars of the earth",
+//   movie: "The Godfather",
+//   game: "Witcher 3",
+//   superHero: "spider man",
+//   randomFact: "I'm disappointed in GOT season 8"
+// }, {
+//   firstName: "Camila",
+//   lastName: "Avina Garcia",
+//   color: "Green",
+//   animal: "Dog",
+//   book: "Match",
+//   movie: "Lady and the Tramp",
+//   game: " Guitar Hero",
+//   superHero: "Spiderman",
+//   randomFact: "I have a twin"
+// }, {
+//   firstName: "Eddson",
+//   lastName: "Jose",
+//   color: "midnight blue",
+//   animal: "pig",
+//   book: "The Dark Forest",
+//   movie: "The Fountain",
+//   game: "Halo Reach",
+//   superHero: "Superman",
+//   randomFact: "I am made up of stardust"
+// }, {
+//   firstName: "Hector",
+//   lastName: "Flores",
+//   color: "Blue",
+//   animal: "Dog",
+//   book: "Captain Underpants",
+//   movie: "Ace Ventura pet detective",
+//   game: "Smash Brothers",
+//   superHero: "Flash",
+//   randomFact: "I am a Libra"
+// }, {
+//   firstName: "Zainib",
+//   lastName: "Ahmed",
+//   color: "Blue",
+//   animal: "Giraffe",
+//   book: "The Book Thief",
+//   movie: "Amelie",
+//   game: "none",
+//   superHero: "The Hulk",
+//   randomFact: "I love to eat olives."
+// }, {
+//   firstName: "Toni",
+//   lastName: "Anguiano",
+//   color: "blue",
+//   animal: "cat",
+//   book: "The Bell Jar",
+//   movie: "Horror",
+//   game: "???",
+//   superHero: "batman",
+//   randomFact: "I like to hangout in cemeteries and research dark California history"
+// }, {
+//   firstName: "Vanessa",
+//   lastName: "Blacow-Berggen",
+//   color: "pink",
+//   animal: "cat",
+//   book: "The Heartstriker series",
+//   movie: "Cats Don't Dance",
+//   game: "Spyro Reignited Trilogy",
+//   superHero: "Spiderman",
+//   randomFact: "I really like cats, especially my cat"
+// }, {
+//   firstName: "Kevin",
+//   lastName: "Sagara",
+//   color: "yellow",
+//   animal: "cat",
+//   book: "sherlock holmes",
+//   movie: "Shaun of the Dead",
+//   game: "osu!",
+//   superHero: "batman",
+//   randomFact: "learning japanese"
+// }, {
+//   firstName: "Anthony",
+//   lastName: "Valdovinos",
+//   color: "purple",
+//   animal: "eagle",
+//   book: "Hatchet",
+//   movie: "Titanic",
+//   game: "Starfox Adventures",
+//   superHero: "none",
+//   randomFact: "I studied Japanese for 4 years"
+// }, {
+//   firstName: "Rachel",
+//   lastName: "Centinaje",
+//   color: "green",
+//   animal: "dogs",
+//   book: "none",
+//   movie: "Drive",
+//   game: "Dishonored",
+//   superHero: "Deadpool",
+//   randomFact: "I went camping this weekend"
+// }, {
+//   firstName: "Alicia",
+//   lastName: "Breinke",
+//   color: "purple",
+//   animal: "dog",
+//   book: "The Art of Racing in the Rain",
+//   movie: "Spirited Away",
+//   game: "???",
+//   superHero: "Wonder Woman",
+//   randomFact: "I want to live in another country someday"
+// }, {
+//   firstName: "asra",
+//   lastName: "siddiqui",
+//   color: "idk",
+//   animal: "chameleons",
+//   book: "codex 1962",
+//   movie: "pulp fiction",
+//   game: "idk",
+//   superHero: "hate em all",
+//   randomFact: "im grumpy"
+// }];
+let roster = [];
+let randomIndex;
+let animating = false;
+let theMagicHour = [];
+let presto = 0;
+let peopleButton;
+let chapterButton;
+let pieceOfPaper;
+let putNameHere = [];
+let premiere = true;
+
+function preload() {
+  for (let i = 0; i <= 15; i++) {
+    theMagicHour[i] = loadImage(`assets/Harry Houdini ${i}.PNG`);
+  }
+}
 
 function setup() {
-  createCanvas(400, 400);
-  background(200);
+  pieceOfPaper = createCanvas(windowWidth, windowHeight);
+  pieceOfPaper.parent("#canvasDiv");
+  background("#858FFF");
+  strokeWeight(10);
+  line(windowWidth / 8, 68, windowWidth / 8, windowHeight - 68);
+  line(windowWidth / 4, 34, windowWidth / 4, windowHeight - 34);
+  line(windowWidth * 3 / 8, 68, windowWidth * 3 / 8, windowHeight - 68);
+  line(windowWidth / 2, 34, windowWidth / 2, windowHeight - 34);
+  line(windowWidth * 5 / 8, 68, windowWidth * 5 / 8, windowHeight - 68);
+  line(windowWidth * 3 / 4, 34, windowWidth * 3 / 4, windowHeight - 34);
+  line(windowWidth * 7 / 8, 68, windowWidth * 7 / 8, windowHeight - 68);
+  stroke("#ffd700");
+  textSize(180);
+  fill('#662d91');
+  imageMode(CENTER);
+  frameRate(8);
 
-  console.log(random(roster));
+  text("Enjoy Harold &", 25, 325);
+  text("Chi's Fairy Tale", 25, 525);
+
+  // button = createButton("Turn the page.");
+  chapterButton = select("#bedTimeStory");
+  chapterButton.mousePressed(buttonPressed);
+  chapterButton.position(windowWidth / 2, windowHeight - chapterButton.height);
+  peopleButton = select("#theProtagonists");
+  peopleButton.mousePressed(theMoreTheMerrier);
+  peopleButton.position(windowWidth / 2 - peopleButton.width, windowHeight - peopleButton.height);
+  for (let i = 0; i < 3; i++) {
+    putNameHere.push(createInput());
+    putNameHere[putNameHere.length - 1].parent("#characterNames");
+  }
 }
 
 function draw() {
-  background(220);
+  if (animating == true) {
+    clear();
+    image(theMagicHour[presto], windowWidth / 2, windowHeight / 2);
+    if (presto < theMagicHour.length - 1) {
+      presto++;
+      console.log(presto);
+    } else {
+      presto = 0;
+    }
+  }
+}
+
+function theMoreTheMerrier() {
+  putNameHere.push(createInput());
+  putNameHere[putNameHere.length - 1].parent("#characterNames");
+}
+
+function randomizer() {
+  animating = false;
+  if (roster[0]) {
+    // background(random(204, 255));
+    clear();
+    randomIndex = int(random(roster.length));
+    // console.log(roster[randomIndex]);
+    stroke(0);
+    fill(255);
+    image(random(theMagicHour), windowWidth / 2, windowHeight / 2);
+    text(roster[randomIndex], 10, 400);
+    roster.splice(randomIndex, 1);
+  } else {
+    background(random(204, 255));
+    noStroke();
+    fill('#662d91');
+    text("The End.", 10, 400);
+  }
+}
+
+function buttonPressed() {
+  if (premiere) {
+    for (let i = 0; i < putNameHere.length; i++) {
+      roster.push(putNameHere[i].value());
+    }
+    premiere = false;
+  }
+  animating = true;
+  setTimeout(randomizer, 2000);
 }
